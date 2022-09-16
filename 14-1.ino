93c13cfe883ac80ba2ce560bc41a44682c3e978f
simoonkim <simoon06@khu.ac.kr>
2022-09-17 05:52:06 +0900

    no message

---
+++ 14-1.ino
@@ -0,0 +1,21 @@
+#include <Servo.h>
+
+Servo myServo;
+int servoPin =3;
+
+void setup(){
+  myServo.attach(servoPin);
+}
+
+void loop(){
+  int angle;
+
+  for (angle=0; angle<180;angle++){
+    myServo.write(angle);
+    delay(5);
+  }
+  for (angle=180;angle>0;angle--){
+    myServo.write(angle);
+    delay(5);
+  }
+}
